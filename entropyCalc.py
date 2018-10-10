ec4f8a085e6a9a701ed022a703177501e8095c1d
Hassan A <hassan@alsaffar.me>
2018-10-09 22:38:47 -0400

    Update entropyCalc.py

--- entropyCalc.py
+++ entropyCalc.py
@@ -4,7 +4,7 @@ import math
 #Input
 url = raw_input("URL: ")
 url = list(url) #seperate each char
-alphabet = list(Set(url)) # list of symbols in the string
+alphabet = list(set(url)) # list of symbols in the string
 
 # calculate the frequency of each symbol in the string
 freqList = []
