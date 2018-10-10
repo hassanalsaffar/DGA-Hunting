2555cab62629260034e6c7848eb2e5e3dd81725b
Hassan Alsaffar <hassan@alsaffar.me>
2018-10-09 22:21:45 -0400

    first commit

---
+++ entropyCalc.py
@@ -0,0 +1,22 @@
+# Shannon Entropy Calculator
+import math
+from sets import Set
+url = raw_input("URL: ")
+url = list(url) #seperate each char
+alphabet = list(Set(url)) # list of symbols in the string
+
+# calculate the frequency of each symbol in the string
+freqList = []
+for symbol in alphabet:
+    ctr = 0
+    for sym in url:
+        if sym == symbol:
+            ctr += 1
+    freqList.append(float(ctr) / len(url))
+
+# Shannon entropy
+ent = 0.0
+for freq in freqList:
+    ent = ent + freq * math.log(freq, 2)
+ent = -ent
+print 'Shannon entropy: %s' % ent
